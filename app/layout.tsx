33d568db5943bade3258679125971fd55fc70868
Andres Malvestiti <andres.malvestiti@gmail.com>
2025-08-18 23:41:07 -0300

    feat: added basic styles and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
+import Header from './components/Header'
 import './globals.css'
+import { Inter, Fira_Code } from 'next/font/google'
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const firaCode = Inter({ subsets: ["latin"], variable: "--font-fira-code" });
 
 export default function RootLayout({
   children,
@@ -6,8 +11,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${inter.className} dark`}>
+      <body className='bg-dark-custom text-foreground scroll-smooth'>
+        <Header/>
+        {children}
+      </body>
     </html>
   )
 }
